b07bc6a44f76b062382cc1963ea0bda48a5ceb7c
Alessandro Muzzi <alessandro.muzzi@verses.ai>
2026-02-16 18:05:12 +0100

    Add config hashBasedRouting

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { init, track } from '@plausible-analytics/tracker'
 init({
   domain: 'portfolio-dev.casabrignuzzi.com.es',
   endpoint: 'https://plausible-tracker.casabrignuzzi.com.es/api/event',
+  hashBasedRouting: true,
   captureOnLocalhost: true, // Useful for testing if your dev env is localhost
 })
 
